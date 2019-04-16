9e684dccc9437650f8c699d6a361b08f253ba48f
chrigkou <christinagoutr@gmail.com>
2019-04-16 20:57:07 +0300

    added dictionary and sorted lists creation to open_file

--- open_file.py
+++ open_file.py
@@ -1,17 +1,25 @@
 import pandas as pd
+from sortedcontainers import SortedList
 
-
-data = pd.read_csv('data-discrete.csv',delimiter = ';',skiprows = 1, na_values = ['no info', '.'],names= ['country', 'sex', 'age', 'suicides_no', 'generation'] )
+data = pd.read_csv('data-discrete.csv',delimiter = ';', na_values = ['no info', '.'] )
 print(data.head(5))
 print(data.columns)
+num_cols= len(data.columns)
+print(num_cols)
 
-print(data['suicides_no'].dtypes)
-print(data['age'].dtypes)
+listed_data = []
+for index in range(num_cols-1):
+    list = SortedList()   #create list for the attribute
+    print(data.columns[index])
+    for i in range(len(data[data.columns[index]])):  #for each row
+        list1 = [data[data.columns[index]][i], i]
+        list.add(list1)
+    print(list)
+    listed_data.append(list) #add the new list to listed_data
+    #print(listed_data[index])
 
+dictionary = dict()
+for index in range(len(data[data.columns[index]])):
+    dictionary.update({index : data[data.columns[num_cols-1]][index]})
 
-list_age = []
-for i in range(len(data['age'])):
-    list1 = [data['age'][i],i+1]
-    list.append(list1)
-print(list)
-
+print(dictionary)
